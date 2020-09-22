430e66460c55abc633aeeb31ce0cbf8b260f6a74
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2020-09-22 10:41:32 -0400

    updates

---
+++ src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
@@ -0,0 +1,8 @@
+package cd.get.ready.algorithms;
+
+public class WuTangForeverBuzz {
+
+    public String wuTangClan(){
+        return null;
+    }
+}
---
+++ src/test/java/cd/get/ready/algorithms/WuTangForeverBuzzTest.java
@@ -0,0 +1,17 @@
+package cd.get.ready.algorithms;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WuTangForeverBuzzTest {
+
+    @Test
+    public void wuTangClanTest(){
+        WuTangForeverBuzz wuTangForeverBuzz = new WuTangForeverBuzz();
+
+        String expected = "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTangForever\n";
+        String actual = wuTangForeverBuzz.wuTangClan();
+
+        Assert.assertEquals(expected,actual);
+    }
+}
