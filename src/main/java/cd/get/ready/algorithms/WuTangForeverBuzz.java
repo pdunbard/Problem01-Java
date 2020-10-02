08d60b13c9213132274bb9c676d28dc7220593fd
Peter R Dunbard <pdunbard1@gmail.com>
2020-10-02 17:07:47 -0400

    this is for assignment 1

--- src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
@@ -2,7 +2,25 @@ package cd.get.ready.algorithms;
 
 public class WuTangForeverBuzz {
 
-    public String wuTangClan(int value){
-        return null;
+    public String wuTangClan(int value) {
+        int input = 15;
+        String out = "" +
+                "";
+        for (int i = 1; i <= input; i++) {
+            if (i % 3 == 0 && i % 5 == 0){
+                out +="WuTangForever\n";
+            }
+            else if(i % 3 == 0) {
+                out +="Wu\n";
+            } else if (i % 5 == 0) {
+                out +="Tang\n";
+
+            } else {
+                out+=i+"\n";
+            }
+
+        }
+
+        return out;
     }
 }
--- src/test/java/cd/get/ready/algorithms/WuTangForeverBuzzTest.java
+++ src/test/java/cd/get/ready/algorithms/WuTangForeverBuzzTest.java
@@ -3,9 +3,11 @@ package cd.get.ready.algorithms;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class WuTangForeverBuzzTest {
-
+public class WuTangForeverBuzzTest{
+    @Test
     public void wuTangClanTest(){
+
+
         WuTangForeverBuzz wuTangForeverBuzz = new WuTangForeverBuzz();
 
         String expected = "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTangForever\n";
@@ -13,8 +15,9 @@ public class WuTangForeverBuzzTest {
 
         Assert.assertEquals(expected,actual);
     }
-
+@Test
     public void wuTangClanTest2(){
+
         WuTangForeverBuzz wuTangForeverBuzz = new WuTangForeverBuzz();
         String expected = "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTangForever\n16\n17\nWu\n19\nTang\nWu\n22\n"+
                 "23\nWu\nTang\n26\nWu\n28\n29\nWuTangForever\n31\n32\nWu\n34\nTang\nWu\n37\n38\nWu\nTang\n";
